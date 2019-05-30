4613636c6bd35e0945b87a322d2160fbc33f1182
wodooWindows <insoul_net@qq.com>
2019-05-30 15:34:26 +0800

    完善通知的理解

--- src/main/java/com/xywei/aop/UserServiceAdvice.java
+++ src/main/java/com/xywei/aop/UserServiceAdvice.java
@@ -11,28 +11,28 @@ public class UserServiceAdvice {
 	}
 
 	public void userAfterRetruning(User result) {
-		System.out.println("后置通知，有返回值resutl=" + result);
+		System.out.println("后置通知，有异常就不执行，有返回值resutl=" + result);
 	}
 
 	public void userAround(ProceedingJoinPoint proceedingJoinPoint) {
-		System.out.println("环绕通知只想方法前");
+		System.out.println("环绕通知执行方法前");
 		try {
 			proceedingJoinPoint.proceed();
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
-		System.out.println("环绕通知只想方法后");
+		System.out.println("环绕通知执行方法后，方法执行了，只是目标对象方法报错不执行");
 	}
 
 	public void userAfter() {
-		System.out.println("最终通知");
+		System.out.println("最终通知，目标类有异常也会执行");
 	}
 
 	public void userAfterThrowing(Throwable throwable) {
 
-		System.out.println("异常通知，有返回异常信息");
-		System.out.println("异常类："+throwable.getClass().getName());
-		System.out.println("异常原因:"+throwable.getMessage());
+		System.out.println("异常抛出通知，有返回异常信息");
+		System.out.println("异常类：" + throwable.getClass().getName());
+		System.out.println("异常原因:" + throwable.getMessage());
 	}
 
 }
--- src/main/java/com/xywei/service/impl/UserServiceImpl.java
+++ src/main/java/com/xywei/service/impl/UserServiceImpl.java
@@ -7,7 +7,6 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public void saveUser() {
-
 		System.out.println("==保存用户==");
 	}
 
